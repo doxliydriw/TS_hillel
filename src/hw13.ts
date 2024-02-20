f31c25a66055a6fe47b5490948c4384743952043
doxliydriw <123301390+doxliydriw@users.noreply.github.com>
2024-02-20 16:55:30 +0100

    partial update

--- src/hw13.ts
+++ src/hw13.ts
@@ -1,123 +1,203 @@
-class Note
-{
-    constructor (
-        public id: number,
-        public title: string,
-        public content: string,
-        public accessStatus: boolean,
-        public createDate: Date = new Date(),
-        public editedDate: Date = new Date(),
-        public status: boolean = false
-    ) { }
+type uuid = number;
 
-    changeStatus ( newStatus: boolean ): void
+interface INote
+{
+    id: uuid,
+    title: string,
+    content: string,
+    createDate: Date,
+    editedDate: Date | null;
+    completed: boolean;
+    update: ( payload: INoteUpdated ) => void;
+    complete: () => void;
+}
+
+type INoteUpdated = Partial<Pick<INote, 'title' | 'content'>>;
+
+interface AdditionalNoteProps
+{
+    changeable: boolean;
+}
+
+abstract class BaseNote<T extends Partial<AdditionalNoteProps> = {}> implements INote
+{
+    readonly id: uuid = Math.random() * ( 100 - 0 ) + 0;
+    readonly createDate = new Date();
+    editedDate: Date | null = null;
+    completed = false;
+    protected additionalProps: T;
+
+    constructor ( public title: string, public content: string, additionalProps: T = {} as T )
     {
-        this.status = newStatus;
+        this.additionalProps = additionalProps;
+    }
+
+    public complete (): void
+    {
+        this.completed = true;
+    }
+
+    public abstract update ( { title, content }: INoteUpdated ): void;
+}
+
+class Note extends BaseNote
+{
+    public update ( { title, content }: INoteUpdated ): void
+    {
+        if ( title?.trim() ) {
+            this.title = title;
+        }
+        if ( content?.trim() ) {
+            this.content = content;
+        }
+        this.editedDate = new Date();
     }
 }
 
-class ToDoList
+class NoteConfirm extends BaseNote<AdditionalNoteProps>
 {
-    private notes: Note[];
-
-    constructor ()
+    public update ( { title, content }: INoteUpdated ): void
     {
-        this.notes = [];
-    }
-
-    addNewNote ( title: string, content: string, accessStatus: boolean ): void
-    {
-        if ( content.length < 0 ) {
-            const id = this.notes.length + 1;
-            const newNote = new Note( id, title, content, accessStatus );
-            this.notes.push( newNote );
-        }
-        throw new Error( 'Note could not be empty' );
-    }
-
-    removeNoteById ( id: number ): void
-    {
-        this.notes = this.notes.filter( note => note.id !== id );
-    }
-
-    modifyNote ( id: number, title: string, content: string, accessStatus: boolean ): void
-    {
-        const index = this.notes.findIndex( note => note.id === id );
-        if ( index !== -1 ) {
-            if ( this.notes[index].accessStatus ) {
-                this.notes[index].title = title;
-                if ( content.length > 0 ) {
-                    this.notes[index].content = content;
-                    this.notes[index].accessStatus = accessStatus;
-                    this.notes[index].editedDate = new Date();
-                }
-                throw new Error( 'Note could not be empty' );
+        if ( this.additionalProps && this.additionalProps.changeable ) {
+            if ( title?.trim() ) {
+                this.title = title;
             }
-            throw new Error( 'This Note could not be edited' );
+            if ( content?.trim() ) {
+                this.content = content;
+            }
+            this.editedDate = new Date();
         }
+        throw new Error( `This note could not be changed` );
     }
-
-    getNoteDetailsById ( id: number ): Note | undefined
-    {
-        return this.notes.find( note => note.id === id );
-    }
-
-    getAllNotes (): Note[]
-    {
-        return this.notes;
-    }
-
-    changeNoteStatusById ( id: number, newStatus: boolean ): void
-    {
-        const note = this.getNoteDetailsById( id );
-        if ( note ) {
-            note.changeStatus( newStatus );
-        } else {
-            console.log( `Note with ID ${id} not found.` );
-        }
-    }
+}
 
 
-    getNotesQuantity (): number
+const test = new Note( 'title', 'content' );
+const testconfirm = new NoteConfirm( 'title', 'content', { changeable: true } );
+
+
+interface iToDoList
+{
+    allCount: number;
+    incompletedCount: number;
+
+    create: ( title: string, content: string ) => void;
+    removeNoteById: ( id: uuid ) => INote;
+    modifyNote: ( id: uuid, payload: INoteUpdated ) => INote;
+    getNoteById: ( id: uuid ) => INote;
+    getAllNotes: () => INote[];
+    complete: ( id: uuid ) => void;
+}
+
+class ToDoList implements iToDoList
+{
+    private notes: INote[] = [];
+
+    get allCount (): number
     {
         return this.notes.length;
     }
 
-
-    countNotesWithFalseStatus (): number
+    get incompletedCount (): number
     {
-        return this.notes.filter( note => !note.status ).length;
+        return this.notes.filter( ( x ) => !x.completed ).length;
     }
 
+    public create ( title: string, content: string ): void
+    {
+        if ( !title.trim() || !content.trim() ) {
+            throw new Error( 'Note could not be empty' );
+        }
+        const newNote = new Note( title, content );
+        this.notes.push( newNote );
+    }
 
-    noteSearch ( query: string ): Note[]
+    public removeNoteById ( id: uuid ): INote
+    {
+        const noteIndex = this.getNoteIndex( id );
+        const [removedNote] = this.notes.splice( noteIndex, 1 );
+        return removedNote;
+    }
+
+    public modifyNote ( id: uuid, payload: INoteUpdated ): INote
+    {
+        const index = this.notes.findIndex( note => note.id === id );
+        const note = this.notes[index];
+        const oldNote = { ...note };
+        note.update( payload );
+        return oldNote;
+    }
+
+    public getNoteById ( id: uuid ): INote
+    {
+        const note = this.notes[this.getNoteIndex( id )];
+        if ( !note ) {
+            throw new Error( `No Note found` );
+        }
+        return note;
+    }
+
+    public getNoteIndex ( id: uuid ): number
+    {
+        const noteIndex = this.notes.findIndex( ( x ) => x.id === id );
+        if ( ~~noteIndex ) {
+            throw new Error( `$(id) is not defined` );
+        }
+        return noteIndex;
+    }
+
+    public getAllNotes (): INote[]
+    {
+        return this.notes;
+    }
+
+    public getNotesQuantity (): number
+    {
+        return this.notes.length;
+    }
+
+    public complete ( id: uuid ): void
+    {
+        const noteIndex = this.getNoteIndex( id );
+        const note = this.notes[noteIndex];
+        note.complete();
+    }
+
+    /*
+        countNotesWithFalseStatus(): number;
+        {
+            return this.notes.filter( note => !note.status ).length;
+        }
+    
+    
+    noteSearch( query: string ): Note[];
     {
         const searchResults: Note[] = [];
-
+    
         for ( const note of this.notes ) {
             if ( note.title.includes( query ) || note.content.includes( query ) ) {
                 searchResults.push( note );
             }
         }
-
+    
         return searchResults;
     }
-
-    sortNotes ( sortBy: 'status' | 'date' ): void
-    {
-        if ( sortBy === 'status' ) {
-            this.notes.sort( ( a, b ) =>
-            {
-                if ( a.status === b.status ) {
-                    return a.createDate.getTime() - b.createDate.getTime();
-                }
-                return a.status ? 1 : -1;
-            } );
-        } else if ( sortBy === 'date' ) {
-            this.notes.sort( ( a, b ) => a.createDate.getTime() - b.createDate.getTime() );
-        } else {
-            throw new Error( 'Please insert date or status' );
-        }
+    
+    sortNotes( sortBy: 'status' | 'date' ): void
+        {
+            if ( sortBy === 'status' ) {
+        this.notes.sort( ( a, b ) =>
+        {
+            if ( a.status === b.status ) {
+                return a.createDate.getTime() - b.createDate.getTime();
+            }
+            return a.status ? 1 : -1;
+        } );
+    } else if ( sortBy === 'date' ) {
+        this.notes.sort( ( a, b ) => a.createDate.getTime() - b.createDate.getTime() );
+    } else {
+        throw new Error( 'Please insert date or status' );
     }
-
+        }
+    */
 }
